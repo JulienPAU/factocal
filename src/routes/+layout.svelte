61d1c29bf8185bd451aef3b46614837f02101748
Elyjun <julien.paulus@oclock.school>
2025-04-08 20:01:07 +0200

    add logo and link to footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,8 @@
     
     <slot />
     
-    <footer class="bg-gray-200 p-4 text-center text-gray-600 mt-10">
+    <footer class="bg-gray-200 p-4 text-center text-gray-600 mt-10 flex justify-center items-center">
       <p>© {new Date().getFullYear()} - Application de gestion de factures</p>
+      <a href="https://julienpaulus.vercel.app/" target="_blank"><img src="/logo_new.svg" alt="logo" class="w-[3.5rem]"></a>
     </footer>
   </div>
